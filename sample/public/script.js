e1d70404f79ddaeb58ad2e52f90df3193fb7317e
always-reach <67514209+always-reach@users.noreply.github.com>
2021-03-21 22:58:02 +0900

    react and bootstrap train

---
+++ sample/public/script.js
@@ -0,0 +1,18 @@
+let dom =document.querySelector('#root')
+
+let element=React.createElement(
+    'div',{},[
+        React.createElement(
+            'h2',{},"Hello!!"),
+            React.createElement(
+                'h4',{className:'alert alert-primary'},"React sample page."
+                ),
+            React.createElement('ul',{className:'list-group'},[
+                React.createElement('li',{className:'list-group-item'},'First Item'),
+                React.createElement('li',{className:'list-group-item'},'Second Item'),
+                React.createElement('li',{className:'list-group-item'},'Thirt Item')
+            ])
+    ]
+)
+
+ReactDOM.render(element,dom)
