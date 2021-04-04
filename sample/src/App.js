24e9fcd1faf89dd89ee68be04145490123b73f95
always-reach <67514209+always-reach@users.noreply.github.com>
2021-04-04 18:17:30 +0900

    component form

--- sample/src/App.js
+++ sample/src/App.js
@@ -2,56 +2,112 @@ import React,{Component} from 'react'
 import './App.css';
 import Rect from './React'
 
-class App extends Component{
-  data=[]
-  area={
-    width:"500px",
-    height:"500px",
-    border:"1px solid blue"
+let theme={
+  light:{
+    styles:{
+      backgroundColor:"#f0f9ff",
+      color:"#00f",
+    },
+    head:"bg-primary text-white display-4 mt-4",
+    alert:"alert alert-primary my-3",
+    text:"text-primary m-3",
+    foot:"py-4"
+  },
+  dark:{
+    styles:{
+      backgroundColor:"#336",
+      color:"#eef",
+    },
+    head:"bg-primary text-white display-4 mt-4",
+    alert:"alert alert-dark my-3",
+    text:"text-light m-3",
+    foot:"py-4"
   }
+}
+
+const ThemeContext=React.createContext(theme.dark)
+class App extends Component{
+  static contextType=ThemeContext
+  input=""
 
   constructor(props){
     super(props)
     this.state={
-      list:this.data
+      title:"input form",
+      message:"type your name.",
+      max:10
     }
-    this.doAction=this.doAction.bind(this)
+    
+    this.doCheck=this.doCheck.bind(this)
+    this.doChange=this.doChange.bind(this)
+    this.doSubmit=this.doSubmit.bind(this)
   }
 
-  doAction(e){
-    let x=e.pageX
-    let y=e.pageY
-    this.data.push({x:x,y:y})
+  doCheck(event){
+    alert(event.target.value+"は長過ぎます。(最大"+this.state.max+"文字)")
+  }
+
+  doChange(event){
+    this.input=event.target.value
+  }
+
+  doSubmit(event){
     this.setState({
-      list:this.data
+      title:'send form',
+      message:'Hello,'+this.input+"!"
     })
-  }
-
-  draw(d){
-    let s={
-      position:"absolute",
-      left:(d.x-25)+"px",
-      top:(d.y-25)+"px",
-      width:"50px",
-      height:"50px",
-      backgroundColor:"#66f3"
-    }
-    return <div style={s}></div>
+    event.preventDefault()
   }
 
   render(){
-    return <div>
-      <h1 className="bg-primary text-white display-4">React</h1>
+    return <div style={this.context.styles}>
+      <h1 className={this.context.head}>React</h1>
       <div className="container">
-        <p className="subtitle">drow rectangle.</p>
-        <div style={this.area} onClick={this.doAction}>
-          {this.data.map((value)=>this.draw(value))}
+        <h4>{this.state.title}</h4>
+        <p className="card h5 p-3">{this.state.message}</p>
+        <Message maxlength={this.state.max} onCheck={this.doCheck} />
+
+        <div className="alert alert-primary mt-3">
+          <form onSubmit={this.doSubmit}>
+            <div className="form-group">
+              <label>Message:</label>
+              <input type="text" className="form-control" onChange={this.doChange} required pattern="[A-Za-z _++]+"/>
+            </div>
+            <input type="submit" className="btn btn-primary" valeu="Click"/>
+          </form>
         </div>
+        <div className={this.context.foot}></div>
       </div>
     </div>
   }
-
-
 }
+
+class Message extends Component{
+  li={
+    fontSize:"14pt",
+    fontWeight:"bold",
+    color:"#090"
+  }
+
+  constructor(props){
+    super(props)
+    this.doChange=this.doChange.bind(this)
+  }
+
+  doChange(event){
+    if(event.target.value.length>this.props.maxlength){
+      this.props.onCheck(event)
+      event.target.value=event.target.value.substr(0,this.props.maxlength)
+    }
+  }
+
+  render(){
+    return <div calssName="form-group">
+      <label>input:</label>
+      <input type="text" className="form-control" onChange={this.doChange}/>
+    </div>
+  }
+}
+
 
 export default App;
